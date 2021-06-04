076aaa2292066e77389a595bff11fa8eb949af7f
Lau <lmarcos@LT-ACER-ISA.localdomain>
2021-06-04 15:19:20 +0200

    first iterations done

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,56 @@
+//IT 1 
+
 // Soldier
-class Soldier {}
+
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    
+    let soldierHealth = this.strength
+    console.log('Your strenght is ${soldierHealth}')
+  };
+
+  receiveDamage(theDamage) {
+    
+  }
+};
+
+
+
+//IT 2 
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+
+  constructor(health, strength, name) {
+
+    super();
+    this.name = "name";
+  }
+}
+
+/*A Viking is a Soldier with an additional property, their name. They also have a different receiveDamage() method and new method, battleCry().
+
+Modify the Viking class, have it inherit from Soldier, re-implement the receiveDamage() method for Viking, and add a new battleCry() method.
+
+inheritance
+Viking should extend Soldier
+class
+should receive 3 arguments (name, health & strength)
+should receive the name property as its 1st argument
+should receive the health property as its 2nd argument
+should receive the strength property as its 3rd argument*/
+
+
 
 // Saxon
-class Saxon {}
+class Saxon { }
 
 // War
-class War {}
+class War { }
 
 
 
